c8d98739629d8d493889bae32ed2383b1014432f
Jan <idiomasjanlan@gmail.com>
2024-10-14 11:32:36 -0300

    mi primer intento de Java

---
+++ CalculadoraBasica.java
@@ -0,0 +1,20 @@
+public class CalculadoraBasica {
+    public static void main(String[] args) {
+        // Declaración de variables
+        int numero1 = 10;
+        int numero2 = 5;
+
+        // Operaciones aritméticas
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        int division = numero1 / numero2;
+
+        // Imprimir resultados
+
+        System.out.println("Suma : " + suma);
+        System.out.println("Resta " + resta);
+        System.out.println("Multiplicacón: " + multiplicacion);
+        System.out.println("División: " + division);
+    }
+}
